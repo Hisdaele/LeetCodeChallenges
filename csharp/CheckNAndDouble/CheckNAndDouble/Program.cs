4c284f1897865b0868adc11ea25baa7ea793ccec
Hisdaele Kadhisk <hisdaele.kamga@gmail.com>
2023-02-06 14:06:42 +0100

    Check If N and Its Double Exist problem with complexity o(n^2)

---
+++ csharp/CheckNAndDouble/CheckNAndDouble/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CheckNAndDouble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 2, 5, 3 };
+            bool hasDouble = CheckIfExist(arr);
+            Console.WriteLine(hasDouble);
+        }
+
+        private static bool CheckIfExist(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for(int j = i+1; j < arr.Length; j++)
+                {
+                    if(arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
