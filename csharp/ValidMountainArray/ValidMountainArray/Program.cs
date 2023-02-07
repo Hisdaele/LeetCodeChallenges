45cd4bc629ff61aa68c7ea9c5d733888f144aa13
Hisdaele Kadhisk <hisdaele.kamga@gmail.com>
2023-02-07 15:14:21 +0100

    resolve valid mountain array problem

---
+++ csharp/ValidMountainArray/ValidMountainArray/Program.cs
@@ -0,0 +1,43 @@
+﻿
+namespace ValidMountainArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3,5,5};
+            bool ans = CheckMountainArray(arr);
+            Console.WriteLine(ans);
+    }
+
+        private static bool CheckMountainArray(int[] arr)
+        {
+            if(arr.Length < 3) { return false; }
+
+            //Looking for top index
+            int top = 0;
+            for(; top < arr.Length - 1; top++) 
+            {
+                if (arr[top] > arr[top+1])
+                {
+                    break;
+                }else if(arr[top] == arr[top + 1])
+                {
+                    return false;
+                }
+            }
+
+            for (; top < arr.Length - 1; top++)
+            {
+                if (arr[top] <= arr[top + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+
+        }
+    }
+}
