60319d1ba6e79a8440aa1b8c2764529864f76623
Hisdaele Kadhisk <hisdaele.kamga@gmail.com>
2023-02-10 14:54:45 +0100

    resolve move zeroes problem

---
+++ csharp/MoveZeroes/MoveZeroes/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace MoveZeroes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 0 };
+            MoveZeroes(arr);
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        private static void MoveZeroes(int[] arr)
+        {
+            int writerPointer = 0;
+            int length = arr.Length;
+
+            //Move non null infront
+            for(int i = 0; i < length; i++)
+            {
+                if (arr[i] != 0)
+                {
+                    arr[writerPointer++] = arr[i];
+                }
+            }
+
+            //Complete the rest with zero
+            while(writerPointer < length)
+            {
+                arr[writerPointer++] = 0;
+            }
+        }
+    }
+}
--- csharp/ReplaceElements/ReplaceElements/Program.cs
+++ csharp/ReplaceElements/ReplaceElements/Program.cs
@@ -15,19 +15,8 @@
         private static int[] ReplaceElements(int[] arr)
         {
             int size = arr.Length;
-            /*for(int i = 0; i < size; i++)
-            {
-                int max = -1;
-                for(int j = i+1; j < size; j++)
-                {
-                    if (arr[j] > max)
-                    {
-                        max = arr[j];
-                    }
-                }
-                arr[i] = max;
-            }*/
             int currentMax = -1;
+
             for(int i = size - 1; i >= 0; i--)
             {
                 int tempo = arr[i];
