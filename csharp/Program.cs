7b1bea91b7c71247a7f55e0c3e5527ff47a1978a
Hisdaele Kadhisk <hisdaele.kamga@gmail.com>
2023-02-13 16:41:54 +0100

    create a util class

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -7,14 +7,6 @@
             Console.WriteLine("Just try to make it");
         }
 
-        private static void PrintArray(int[] nums)
-        {
-            Console.Write("[");
-            foreach (int val in nums)
-            {
-                Console.Write($"{val},");
-            }
-            Console.WriteLine("]");
-        }
+        
     }
 }
---
+++ csharp/Utils.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCodeChallenges
+{
+    internal class Utils
+    {
+        private static void PrintArray(int[] nums)
+        {
+            Console.Write("[");
+            foreach (int val in nums)
+            {
+                Console.Write($"{val},");
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
